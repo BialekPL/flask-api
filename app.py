b8a1f6cf7145de97db11ed2888b3b57f0f00d193
BialekPL <mbialecki157@gmail.com>
2024-02-13 17:26:21 +0100

    init commit

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
-import os
-
+import mysql.connector
+from os import getenv, path
 from flask import (Flask, redirect, render_template, request,
-                   send_from_directory, url_for)
+                   send_from_directory, url_for, jsonify)
 
 app = Flask(__name__)
 
+def connect_to_db():
+    app.config['MYSQL_HOST'] = getenv('DB_HOST')
+    app.config['MYSQL_DB'] = getenv('DB_DATABASE')
+    app.config['MYSQL_USER'] = getenv('DB_USERNAME')
+    app.config['MYSQL_PASSWORD'] = getenv('DB_PASSWORD')
+    cnx = mysql.connector.connect(user=getenv('DB_USERNAME'), password=getenv('DB_PASSWORD'),
+                                host=getenv('DB_HOST'),
+                                database=getenv('DB_DATABASE'),
+                                connection_timeout=6)
+    return cnx
 
 @app.route('/')
 def index():
@@ -13,7 +23,7 @@ def index():
 
 @app.route('/favicon.ico')
 def favicon():
-    return send_from_directory(os.path.join(app.root_path, 'static'),
+    return send_from_directory(path.join(app.root_path, 'static'),
                                'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
 @app.route('/hello', methods=['POST'])
@@ -26,7 +36,25 @@ def hello():
    else:
        print('Request for hello page received with no name or blank name -- redirecting')
        return redirect(url_for('index'))
-
+   
+@app.route("/get")
+def get():
+    try: 
+        cnx = connect_to_db()
+        query = ("SELECT FirstName, LastName FROM Persons WHERE PersonID = 1;")
+        cursor = cnx.cursor()
+        cursor.execute(query)
+        name, lastname = cursor.fetchone()
+        user_data = {
+            "name": name,
+            "lastname": lastname
+        }
+        return jsonify(user_data)
+    except:
+        response = {
+            "Error": f"Error when connecting to database {app.config['MYSQL_HOST']}"
+        }
+    return jsonify(response)
 
 if __name__ == '__main__':
    app.run()
